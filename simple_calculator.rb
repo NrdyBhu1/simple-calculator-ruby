4f7e182cc415e09bfa2a1649183e08676a12c279
NrdyBhu1@gmail.com <bunnu16aug>
2020-11-27 14:50:21 +0530

    functions

--- simple_calculator.rb
+++ simple_calculator.rb
@@ -1,2 +1,27 @@
 # Simple calculator written in ruby
 # Source code https://github.com/NrdyBhu1/simple-calculator-ruby/
+
+def getSum pint1 pint2
+    oint3 = pint1.to_i + pint2.to_i
+    puts oint3
+end
+
+def getDiff pint1 pint2
+    oint3 = pint1.to_i - pint2.to_i
+    puts oint3
+end
+
+def getProd pint1 pint2
+    oint3 = pint1.to_i * pint2.to_i
+    puts oint3
+end
+
+def getQuo pint1 pint2
+    oint3 = pint1.to_i / pint2.to_i
+    puts oint3
+end
+
+def getRem pint1 pint2
+    oint3 = pint1.to_i % pint2.to_i
+    puts oint3
+end
